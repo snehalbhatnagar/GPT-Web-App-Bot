c22ea45e431bb3043b03708f87b31df1b0cb3491
Snehal Bhatnagar <bhatnagarsnehal@gmail.com>
2023-06-23 01:00:50 +0530

    'commit3'

---
+++ index.js
@@ -0,0 +1,73 @@
+const conversationElement = document.getElementById('conversation');
+const startButton = document.getElementById('start-btn');
+const stopButton = document.getElementById('stop-btn');
+
+const recognition = new webkitSpeechRecognition();
+
+startButton.addEventListener('click', () => {
+  console.log("Listening...");
+  recognition.start(); // Start speech recognition when the start button is clicked
+});
+
+stopButton.addEventListener('click', () => {
+  console.log("Stopped");
+  recognition.stop(); // Stop speech recognition when the stop button is clicked      
+});
+
+recognition.addEventListener('result', (event) => {
+  const transcript = event.results[0][0].transcript; // Get the transcribed text
+  console.log('Transcript:', transcript);  
+  generateGPTResponse(transcript); // Pass the transcript to the GPT-3.5 processing function
+  displayResponse(transcript, "You: ");
+});
+
+recognition.addEventListener('error', (event) => {
+  console.error('Speech recognition error:', event.error);
+});
+
+recognition.addEventListener('end', () => {
+  console.log("End of speech recognition");      
+});
+
+function generateGPTResponse(prompt) {      
+  const apiKey = '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';
+
+  fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}` // Pass the API key or access token
+    },
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo', // Specify the model name
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: prompt }
+      ]
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log('GPT response:', result);
+    const gptResponse = result.choices[0].message.content; // Extract the GPT response from the API response
+    displayResponse(gptResponse, "Bot: "); // Display the GPT response
+    convertTextToSpeech(gptResponse); // Convert the response to speech
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    // Handle the error
+  });
+}
+
+function displayResponse(response, user) {
+  const conversationElement = document.getElementById('conversation');
+  const responseElement = document.createElement('p');
+  responseElement.textContent = user + response;
+  conversationElement.appendChild(responseElement);
+}
+
+function convertTextToSpeech(text) {
+  const synthesis = window.speechSynthesis;
+  const utterance = new SpeechSynthesisUtterance(text);
+  synthesis.speak(utterance);
+}
--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-
-
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// Set the views directory and the view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Route to handle voice input
 app.post('/voice', (req, res) => {
-    // Process the voice input
-  });
-  
-  // Route to serve the web application
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-  
+  // Process the voice input
+});
 
+// Route to serve the web application
+app.get('/', (req, res) => {
+  res.render('index', { apiKey: process.env.OPENAI_API_KEY });
+});
 
-const port = 3000; // Choose a port number for your server
+// Route to serve the index.js file
+app.get('/index.js', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.js'));
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });

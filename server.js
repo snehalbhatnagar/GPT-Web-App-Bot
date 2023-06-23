f698adfec1a7b3a9663dff995a61968bfd1fdb00
snehalbhatnagar <76249876+snehalbhatnagar@users.noreply.github.com>
2023-06-23 09:39:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set the views directory and the view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');

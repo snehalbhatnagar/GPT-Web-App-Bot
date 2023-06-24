399cb8dc4954c4df0b7eb8ec5c09003a39251c5b
Snehal Bhatnagar <bhatnagarsnehal@gmail.com>
2023-06-25 00:52:56 +0530

    commitw

--- server.js
+++ server.js
@@ -3,24 +3,28 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Set the views directory and the view engine
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // Route to handle voice input
 app.post('/voice', (req, res) => {
   // Process the voice input
 });
 
+
 // Route to serve the web application
 app.get('/', (req, res) => {
-  res.render('index', { apiKey: process.env.OPENAI_API_KEY });
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.get('/api/key', (req, res) => {
-  res.json({ apiKey: process.env.OPENAI_API_KEY });
-});
+
+ app.get('/api/key', (req, res) => {
+   res.json({ apiKey: process.env.OPENAI_API_KEY });
+ });
 
 // Route to serve the index.js file
 app.get('/index.js', (req, res) => {
